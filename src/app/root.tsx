965848135e83fd23a1fd3924c7e8c583233405a9
Advance Servicios Integrales <45326791+michaeliq@users.noreply.github.com>
2026-01-08 09:14:35 -0500

    update

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -12,6 +12,14 @@ import "./app.css";
 import Header from "src/components/Header";
 import Footer from "src/components/Footer";
 
+
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.keys().next().value;
+
+if (redirectPath) {
+  window.history.replaceState(null, "", "/" + redirectPath);
+}
+
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
