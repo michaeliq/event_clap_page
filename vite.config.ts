e743df42364826a93e08ced6baf9015ef6219663
Advance Servicios Integrales <45326791+michaeliq@users.noreply.github.com>
2026-01-08 21:10:09 -0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base:"/event_clap_page",
   build: {
     outDir: "dist",
   },
