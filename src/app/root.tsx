0fc9a8073920cff1d9c503829d1621d1d950afe4
Advance Servicios Integrales <45326791+michaeliq@users.noreply.github.com>
2026-03-12 20:06:46 -0500

    create, whatsapp component

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "src/components/Header";
 import Footer from "src/components/Footer";
+import WhatsAppComponent from "src/components/WhatsAppComponent";
 
 
 if (typeof window !== "undefined") {
@@ -54,6 +55,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <WhatsAppComponent/>
         <ScrollRestoration />
         <Scripts />
       </body>
---
+++ src/components/WhatsAppComponent.jsx
@@ -0,0 +1,13 @@
+"use client"
+import { Link } from "react-router";
+import whatsapp_icon from "../assets/whatsapp.png"
+import "../styles/WhatsApp.css";
+
+
+export default function WhatsAppComponent(){
+    return(
+        <Link to="https://wa.me/573188016709" target="_blank" className="z-10000 whatsapp-comp fixed bottom-5 right-5 ">
+            <img src={whatsapp_icon} alt="WhatsApp Event Clap" className="w-[70px] h-[70px] hover:scale-[1.1]" />
+        </Link>
+    )
+}
